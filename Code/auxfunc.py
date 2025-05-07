5532e24110c24db9172a4718f100ebe1f60b911e
Juanc <jkaz.os@gmail.com>
2025-05-07 10:02:33 -0500

    fixed repo path. Extract with globals repo path variable

--- Code/auxfunc.py
+++ Code/auxfunc.py
@@ -4,10 +4,11 @@ Auxiliary functions
 JCA
 """
 import os
+import Code.globals as gb
 
-def extract_images(repo_path):
+def extract_images():
     """Extract example images from repository"""
-    z = os.path.join(repo_path, 'Dataset', 'Whatsapp30-10.zip')
-    local_path =  os.path.join(repo_path, 'Dataset')
+    z = os.path.join(gb.REPO_PATH, 'Dataset', 'Whatsapp30-10.zip')
+    local_path =  os.path.join(gb.REPO_PATH, 'Dataset')
     os.system(f'unzip -qq "{z}" -d "{local_path}"')
     return local_path
--- Code/globals.py
+++ Code/globals.py
@@ -7,5 +7,5 @@ import os
 from pathlib import Path
 
 script_path, _ = os.path.split(os.path.realpath(__file__))
-script_path = Path(os.path.dirname(script_path))
-REPO_PATH = str(script_path.parent.absolute())
+REPO_PATH = Path(os.path.dirname(script_path))
+# REPO_PATH = str(script_path.parent.absolute())
